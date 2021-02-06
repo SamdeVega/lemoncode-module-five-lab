1f9f8b5689c0a9295a7c30a048284fe21c5a62dc
Samuel de Vega <s.a.devega.garcia@gmail.com>
2021-02-06 13:20:42 +0000

    ✅ add tests to confirm dialog component

---
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -0,0 +1,98 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { ConfirmationDialogComponent } from "./confirmation-dialog.component";
+
+function retrieveButtonFromDialog(
+  button: "accept" | "cancel",
+  action: () => void
+) {
+  const buttons = {
+    accept: function(onAccept: () => void) {
+      render(
+        <ConfirmationDialogComponent
+          isOpen={true}
+          onAccept={onAccept}
+          onClose={() => {
+            return;
+          }}
+          title={""}
+          labels={{ acceptButton: "accept me", closeButton: "" }}
+        />
+      );
+      return screen.getByRole("button", { name: "accept me" });
+    },
+    cancel: function(onClose: () => void) {
+      render(
+        <ConfirmationDialogComponent
+          isOpen={true}
+          onAccept={() => {
+            return;
+          }}
+          onClose={onClose}
+          title={""}
+          labels={{ acceptButton: "cancel me", closeButton: "" }}
+        />
+      );
+      return screen.getByRole("button", { name: "cancel me" });
+    }
+  };
+  return buttons[button](action);
+}
+
+describe("ConfirmationDialogComponent", () => {
+  it("should be rendered closed", () => {
+    render(
+      <ConfirmationDialogComponent
+        isOpen={false}
+        onAccept={() => {
+          return;
+        }}
+        onClose={() => {
+          return;
+        }}
+        title={"a title"}
+        labels={{ acceptButton: "", closeButton: "" }}
+      />
+    );
+
+    const dialogElement = screen.queryByRole("heading", { name: "a title" });
+    expect(dialogElement).not.toBeInTheDocument;
+  });
+
+  it("should be rendered open", () => {
+    render(
+      <ConfirmationDialogComponent
+        isOpen={true}
+        onAccept={() => {
+          return;
+        }}
+        onClose={() => {
+          return;
+        }}
+        title={"a title"}
+        labels={{ acceptButton: "", closeButton: "" }}
+      />
+    );
+
+    const titleElement = screen.getByRole("heading", { name: "a title" });
+    expect(titleElement).toBeInTheDocument;
+  });
+
+  it("should trigger on accept action when accept button is clicked", () => {
+    const onAcceptSpy = jest.fn();
+    const acceptButton = retrieveButtonFromDialog("accept", onAcceptSpy);
+
+    acceptButton.click();
+
+    expect(onAcceptSpy).toHaveBeenCalled();
+  });
+
+  it("should trigger on cancel action when accept button is clicked", () => {
+    const onCancelSpy = jest.fn();
+    const cancelButton = retrieveButtonFromDialog("cancel", onCancelSpy);
+
+    cancelButton.click();
+
+    expect(onCancelSpy).toHaveBeenCalled();
+  });
+});
